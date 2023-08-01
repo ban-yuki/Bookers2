891272cd8a3a4a610b517c016924995198b024b7
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-01 12:02:07 +0000

    [Add]　課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,43 @@
 class BooksController < ApplicationController
-  def new
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end 
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+      redirect_to books_path
   end
-
+  
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+    @books = Book.all
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end 
+  end
+  
+  private
+  
 end
