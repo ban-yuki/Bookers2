6e914997a892e08b06404edcf8ff5524191aaae2
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-06 06:54:59 +0000

    [Add]　課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def show
     @user = User.find(params[:id])
   end
   
   before_action :is_matching_login_user, only: [:edit, :update]
+  
+  def index
+    @user = User.all
+  end
 
   def edit
     @user = User.find(params[:id])
