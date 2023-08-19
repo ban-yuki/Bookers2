bb95bc7197926855e627d7a0b1c224fec520a75a
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-19 06:17:44 +0000

    [Add]　課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    
+
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
 
   def index
     @newbook = Book.new
-    @book = Book.new(params[:id])
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
