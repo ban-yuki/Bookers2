107f7605b79c9a4ad7a19ca8ce8112e99abe4e63
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-19 00:01:05 +0000

    [Add]　課題3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   def after_sign_in_path_for(resource)
     users_path(current_user.id)
-    if @user.save
-      flash[:notice] = "Welcome! You have signed up successfully."
-      redirect_to users_path(current_user.id)
-    else
-      render :index
-    end
+    
   end
 
   def after_sign_out_path_for(resource)
     about_path
   end
-
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  def create
-
-  end
+  
 end
