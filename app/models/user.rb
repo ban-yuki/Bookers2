866e4f69932b195eb17afac4f18c795d960a5154
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-21 11:49:37 +0000

    [Add]　課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, presence: true, length: { maximum: 50 }
+  
   has_one_attached :profile_image
   
   def get_profile_image(width,height)
