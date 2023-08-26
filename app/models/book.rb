d2fece7b3eccc170593c859eaec179bddb323125
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-08-26 11:58:15 +0000

    [Add]　課題3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   #has_one_attached :title, :body
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: true, length: { maximum: 200 }
+  validates :title, presence: true, length: { maximum: 200 }
   validates :body, presence: true, length: { maximum: 200 }
 
 
