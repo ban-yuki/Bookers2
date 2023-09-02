c66042b285fd4065499eef929e810e690221f071
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-09-02 09:05:45 +0000

    [Add]　課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,12 @@ class UsersController < ApplicationController
       redirect_to post_images_path
     end
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      redirect_to user_path(@user.id)
+    end    
   end
   
   private
