5fc2f93e796517b64a530d9b7ca407b6d08a5ce9
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-09-02 10:30:36 +0000

    [Add]　課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
-    @users = User.find(params[:id])
     @newbook = Book.new
-    @user = current_user
+    @user = User.find(params[:id])
+    @books = @user.books
    
     
   end
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
   def is_matching_login_user
