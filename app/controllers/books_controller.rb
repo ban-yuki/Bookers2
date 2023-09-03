6ebc3240a39e8ad61abed9f7c783a7dfa81a6f86
EC2 Default User <ec2-user@ip-172-31-45-112.ap-northeast-1.compute.internal>
2023-09-03 07:08:46 +0000

    [Add]　課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,8 +37,6 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    @books = Book.all
-    @user = User.find(params[:id])
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,52 +5,49 @@ class UsersController < ApplicationController
     @newbook = Book.new
     @user = User.find(params[:id])
     @books = @user.books
-   
-    
   end
-  
+
   def index
     @user = current_user
     @users = User.all
     @newbook = Book.new
     @books = current_user.books
-   
-    
   end
 
   def edit
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(@user.id)
+      redirect_to user_path
     end
     @user = User.find(params[:id])
   end
-  
+
   def update
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to post_images_path
+      redirect_to users_path
     end
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else
-      redirect_to user_path(@user.id)
-    end    
+      user = User.find(params[:id])
+      render :edit
+    end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to user_path(@user.id)
     end
   end
-  
+
 end
